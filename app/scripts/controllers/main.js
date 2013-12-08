c83ce7de53406bb3ea0614479d10f728a3d0c56b
koreafood <koreafood@MacMini.local>
2013-12-08 13:50:21 +0900

    git test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,5 +4,6 @@ angular.module('wowApp')
   .controller('MainCtrl', function ($scope, $http) {
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
+      //ready
     });
   });
